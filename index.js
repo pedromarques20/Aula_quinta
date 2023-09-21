7b4f14d1aeaece0aa81e2f7c7d1dd4f391bbea04
pedromarques20 <pedrohsmarques18@gmail.com>
2023-09-21 10:50:41 -0300

    atualização

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 document.getElementById('botao')
 .addEventListener('click', function(){
-    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    
+    let nome = document.getElementById('nome-pokemon').value
+    
+    fetch('https://pokeapi.co/api/v2/pokemon/'+nome)
     .then(function(response){
         return response.json()
     }).then(function(response){
-        console.log(response)
+        console.log(response.sprites.front_default)
+
+        let url = response.sprites.front_default
+        document.getElementById('img-card').setAttribute('src', url)
     })
 });
